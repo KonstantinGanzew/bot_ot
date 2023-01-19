64851e051cd1cb95f5e3c2422610ed1fdcf05788
Konstantine Gantcev <kanzew@gmail.com>
2023-01-19 23:24:09 +0500

    https://github.com/KonstantinGanzew/bot_ot

--- keybords/inline/choice_buttons.py
+++ keybords/inline/choice_buttons.py
@@ -1,11 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import keybords.inline.callback_datas as key
 
-back_keyboard = InlineKeyboardMarkup(
-    inline_keyboard = [
-        
-    ]
-)
 
 menu_keyboard = InlineKeyboardMarkup(
     inline_keyboard = [
@@ -60,6 +55,46 @@ territory_street_keyboard = InlineKeyboardMarkup(
         ],
         [
             InlineKeyboardButton(text='Назад', callback_data=key.territory_street_callback.new(item_territory_street='back')),
+        ],
+    ]
+)
+
+premises_office_keyboard = InlineKeyboardMarkup(
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(text='Можно поскользнуться, споткнуться', callback_data=key.premises_office_callback.new(item_premises_office='you_can_slip')),
+        ],
+        [
+            InlineKeyboardButton(text='Можно удариться', callback_data=key.premises_office_callback.new(item_premises_office='can_be_hit')),
+        ],
+        [
+            InlineKeyboardButton(text='Можно упасть с высоты', callback_data=key.premises_office_callback.new(item_premises_office='fall_from_a_height')),
+        ],
+        [
+            InlineKeyboardButton(text='Можно обжечься, загореться', callback_data=key.premises_office_callback.new(item_premises_office='you_can_get_burned')),
+        ],
+        [
+            InlineKeyboardButton(text='Можно уколоться, порезаться', callback_data=key.premises_office_callback.new(item_premises_office='prick_cut')),
+        ],
+        [
+            InlineKeyboardButton(text='Может придавить', callback_data=key.premises_office_callback.new(item_premises_office='can_crush')),
+        ],
+        [
+            InlineKeyboardButton(text='Назад', callback_data=key.premises_office_callback.new(item_premises_office='back')),
+        ],
+    ]
+)
+
+out_of_work_keyboard = InlineKeyboardMarkup(
+    inline_keyboard = [
+        [
+            InlineKeyboardButton(text='Неправильно ведутся работы', callback_data=key.out_of_work_callback.new(item_out_of_work='you_can_slip')),
+        ],
+        [
+            InlineKeyboardButton(text='Небезопасное пространство', callback_data=key.out_of_work_callback.new(item_out_of_work='can_be_hit')),
+        ],
+        [
+            InlineKeyboardButton(text='Назад', callback_data=key.out_of_work_callback.new(item_out_of_work='back')),
         ],
     ]
 )
